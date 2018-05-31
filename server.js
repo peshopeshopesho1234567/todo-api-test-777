e89322d1327b3e7e42d286b212bf8441135dec15
Grigor P. Penev <Grigor.Penev@verint.com>
2018-05-31 13:22:54 +0300

    Setup start script and Heroku porst

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require("express");
 
+const port = process.env.PORT || 3000;
+
 const app = express();
 
 app.get("/", (req, res) => {
     res.send("Hello, world!");
 });
 
-app.listen(3000);
+app.listen(port, () => {
+    console.log(`Server is up on port ${port}`);
+});
